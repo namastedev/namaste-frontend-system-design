f004f3a80117c1781d0e014484325636165ad731
Akshay Saini <akshaymarch7@gmail.com>
2024-02-09 20:47:47 +0530

    Multi Language Support

--- LLD/app/src/components/About.js
+++ LLD/app/src/components/About.js
@@ -1,61 +1,25 @@
-const About = () => {
+import { LANG } from "../utils/langConstant";
+
+const About = ({ lang }) => {
+  console.log(lang);
+  const data = LANG[lang];
   return (
     <div>
       <div>
-        <h1 className="font-bold text-2xl my-5">About</h1>
-        <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum.{" "}
-        </p>
+        <h1 className="font-bold text-2xl my-5">{data.title}</h1>
+        <p>{data.desc}</p>
       </div>
       <div>
-        <h1 className="font-bold text-2xl my-5">Mission</h1>
-        <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum.{" "}
-        </p>
+        <h1 className="font-bold text-2xl my-5">{data.title2}</h1>
+        <p>{data.desc}</p>
       </div>
       <div>
-        <h1 className="font-bold text-2xl my-5">About the Product</h1>
-        <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum.{" "}
-        </p>
+        <h1 className="font-bold text-2xl my-5">{data.title3}</h1>
+        <p>{data.desc}</p>
       </div>
       <div>
-        <h1 className="font-bold text-2xl my-5">Address</h1>
-        <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum.{" "}
-        </p>
+        <h1 className="font-bold text-2xl my-5">{data.title4}</h1>
+        <p>{data.desc}</p>
       </div>
     </div>
   );
